b3049e4d1e0206a33808c3d33dca7ccc7260cf24
Anasthesia87 <168677669+Anasthesia87@users.noreply.github.com>
2025-06-17 01:37:11 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,15 +6,11 @@ import uvicorn
 from fastapi import FastAPI
 from app.database.engine import create_db_and_tables
 from app.routers import status, users
-from fastapi_pagination import add_pagination
 
 
 app = FastAPI()
 app.include_router(status.router)
 app.include_router(users.router)
-
-
-add_pagination(app)
 
 
 if __name__ == "__main__":
